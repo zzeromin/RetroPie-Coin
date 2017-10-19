25c370b9723ec2d5640618a29c8fa6548dc24757
Youngmin Seo <zzeromin@gmail.com>
2017-10-19 23:28:43 +0900

    Update retropie-coin.py

--- retropie-coin.py
+++ retropie-coin.py
@@ -13,7 +13,8 @@ import RPi.GPIO as GPIO
 import time
 
 # check your SELECT gpio(BCM) pin number at https://pinout.xyz/
-SELECT = 10 # GPIO BCM Number
+# Joysticks connected to GPIOs at https://github.com/recalbox/mk_arcade_joystick_rpi
+SELECT = 9  # GPIO BCM Number
 SENSOR = 21 # GPIO BCM Number
 
 GPIO.setmode(GPIO.BCM)
@@ -21,13 +22,13 @@ GPIO.setup(SENSOR, GPIO.IN)
 GPIO.setup(SELECT, GPIO.OUT)
 
 try:
-	while True:
-		if GPIO.input(SENSOR) == GPIO.LOW:
-			GPIO.output(SELECT, GPIO.HIGH)
-		else:
-			GPIO.output(SELECT, GPIO.LOW)
-			print "Coin detected"
-			time.sleep(0.05)
+    while True:
+        if GPIO.input(SENSOR) == GPIO.LOW:
+            GPIO.output(SELECT, GPIO.LOW)
+        else:
+            GPIO.output(SELECT, GPIO.HIGH)
+            print "Coin detected"
+            time.sleep(0.05)
 
 except KeyboardInterrupt:
-	GPIO.cleanup()
+    GPIO.cleanup()
