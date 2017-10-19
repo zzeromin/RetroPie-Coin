6413e9ce4aa36042bf097f702e3d3ae6be0a9acd
Youngmin Seo <zzeromin@gmail.com>
2017-10-20 00:46:38 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# How to Setup : 
+# cd /home/pi
+# git clone https://github.com/zzeromin/RetroPie-Coin.git
+# cd RetroPie-Coin/
+# chmod 755 setup.sh
+# sudo ./setup.sh
+
+cd /home/pi/
+git clone https://github.com/zzeromin/RetroPie-Coin.git
+cd RetroPie-Coin/
+cp coin.service /lib/systemd/system/
+systemctl enable coin.service
+echo "RetroPie-Coin setup complete."
+echo "Python-rpi setup is starting now"
+apt-get update
+apt-get -y install python-rpi.gpio
+echo "System will reboot after 3 seconds"
+sleep 3
+reboot
