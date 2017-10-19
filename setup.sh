2b148467dd92dc1cdd4c9d53a449d611c9a1b45c
Youngmin Seo <zzeromin@gmail.com>
2017-10-20 00:51:09 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ cp coin.service /lib/systemd/system/
 systemctl enable coin.service
 echo "RetroPie-Coin setup complete."
 echo "Python-rpi setup is starting now"
-apt-get update
+# apt-get update
 apt-get -y install python-rpi.gpio
 echo "System will reboot after 3 seconds"
 sleep 3
